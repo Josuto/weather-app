62d91ff7e12ba04702383de858f4c8a16e5795a4
Josu Martinez <6097850+Josuto@users.noreply.github.com>
2024-07-31 21:51:42 +0200

    docs: justify the implementation of Municipalities

--- src/types/Municipalities.ts
+++ src/types/Municipalities.ts
@@ -1,5 +1,18 @@
 import { Municipality } from "./Municipality";
 
+/**
+ * Inspired in the Decorator design pattern, this class is a wrapper for an array of municipalities.
+ *
+ * While instances of this class are iterable and include some JS Array logic, I was not very much
+ * interested in implementing the such an interface. Actually, I just wanted to prove a friend of mine
+ * (Aral Roca) the benefits of introducing OO concepts in React apps.
+ *
+ * JS classes are great to encapsulate the logic of the real world objects they model, and if assured
+ * to be inmutable, class instances can be used as state variables. You may think that this class may
+ * look as an over-engineering effort, but since municipalities manipulation is pretty frequently done
+ * in this code base, it well payed its price, as it simplified the implementation of some other code
+ * elsewhere.
+ */
 export class Municipalities {
   private _getMunicipalities: () => Municipality[];
   getLength: () => number;
@@ -28,7 +41,7 @@ export class Municipalities {
 
   [Symbol.iterator](): Iterator<Municipality> {
     let index = 0;
-    const municipalities = this._getMunicipalities(); // Capture the array in a local variable
+    const municipalities = this._getMunicipalities();
 
     return {
       next(): IteratorResult<Municipality> {
